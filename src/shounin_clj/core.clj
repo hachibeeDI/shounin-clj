bf4bbfe0bd935bdb27a7fed2543be871adea2c4f
dogura <daiki.ogura@soliton.co.jp>
2014-11-21 18:29:08 +0900

    initial commit

---
+++ src/shounin_clj/core.clj
@@ -0,0 +1,53 @@
+(ns shounin-clj.core
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojure.data.json :as json]
+            [org.httpkit.client :as http]
+            ))
+
+
+(defn exit-with
+  [st & funcs]
+  (doseq [f funcs] (f))
+  (System/exit st)
+  )
+
+
+(def shounin-options
+  [
+   ["-l" "--list" "list of vocabularies"
+    :default false]
+   ["-a" "--add WORD" "add a new vocabulary"]
+   ["-d" "--delete INDEX" "delete a vocabulary"]
+   ["-h" "--help"]])
+
+
+
+(defn get-vocabularies []
+  (let [response1 (http/get "http://www.shounin.jp/vocabularies")]
+    (for [vocabulary
+          (get (json/read-str (:body @response1)) "vocabularies")]
+      (when-not (= vocabulary nil) (format "%s: %s" (get vocabulary 0) (get vocabulary 1))))))
+
+
+(defn add-new-vocabulary [word]
+  (let [response (http/post "http://www.shounin.jp/vocabularies" {:form-params {:text word}})]
+    (let [{:keys [body]} @response]
+      (get (json/read-str body) "message"))))
+
+
+(defn delete-vocabulary [index]
+  (let [response (http/delete (str "http://www.shounin.jp/vocabularies/" index))]
+    (let [{:keys [body]} @response]
+      (get (json/read-str body) "message"))))
+
+
+(use '[clojure.string :only (join)])
+(defn -main [& args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args shounin-options)]
+    (cond
+      (= (count options) 0) (exit-with 1 (fn [] (println "no arguments")))
+      (:help options) (exit-with 0 #(println summary))
+      (:list options) (exit-with 0 #(println (join "\n" (get-vocabularies))))
+      (:add options) (exit-with 0 #(println (add-new-vocabulary (:add options))))
+      (:delete options) (exit-with 0 #(println (delete-vocabulary (:delete options))))
+      )))
---
+++ test/shounin_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns shounin-clj.core-test
+  (:require [clojure.test :refer :all]
+            [shounin-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
